bf87c3def4e75de8c935e05657673950a3b44ac4
RicardoValadezCTR <60150103+RicardoValadezCTR@users.noreply.github.com>
2020-01-21 11:54:39 -0600

    Update index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<header>
+  <ol>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Quienes Somos</a></li>
+    <li><a href="#">Sucursales</a></li>
+    <li><a href="#">Contacto</a></li>
+  </ol>
+</header>
